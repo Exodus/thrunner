1ea05a0de23ab3e0d0aefc4d1be74cd513a5bcbe
Miguel A. Alvarado V <alvaradoma@gmail.com>
2017-05-29 14:06:36 -0700

    Update thrunner.py

--- thrunner.py
+++ thrunner.py
@@ -1,59 +1,76 @@
 #!/usr/bin/env python
-# A Python script for running threaded shell executions with variable replacement fed through a file
-# Miguel A. Alvarado V.
 import argparse
 import shlex
-from threading import Thread
 import Queue
+from os import path
+from threading import Thread
 from subprocess import Popen, PIPE
 
-parser = argparse.ArgumentParser(description='Multiple Threads Runner')
-parser.add_argument('serverlist', type=file, help='')
-parser.add_argument('params', help='Parameters used to exec')
-parser.add_argument('-t', '--threads', type=int, help='Amount of threads to spawn')
-parser.parse_args()
 
-# Initializing some global objects
-threads = parser.threads
-params = parser.params
-hosts = parser.serverlist.read().split()
+#Global Initiators
 hostq = Queue.Queue()
 logq = Queue.Queue()
-msgs = []
+log = []
 
+#Create the argument parser for options
+parser = argparse.ArgumentParser(description='Create threads for work on multiple servers')
+parser.add_argument('serverlist', type=file, help='File with the list of servers')
+parser.add_argument('params', help='quoted string with variable %var% to replace with host')
+parser.add_argument('-t', '--threads', type=int, default=2, help='Amount of threads to create')
+parser.add_argument('-e', '--email', help='Email the output to <email>')
+parser.add_argument('-o', '--output', help='Output file for processing')
 
+# Python version argparse issue with a file not found
+try:
+    arg = parser.parse_args()
+except IOError:
+    exit('Host file not found')
 
-# Worker thread: thread # and queue.
-def runner(id, queue):
+# Parsing the parameters
+params = shlex.split(arg.params)
+params[0] = path.abspath(params[0])
+if not any('%var%' in param for param in params):
+    exit('%var% not present')
+if not path.isfile(params[0]):
+    exit('Executable file not found')
+
+# Getting the hosts from a file
+hosts = arg.serverlist.read().split()
+
+# Thread running process
+def check_cert(i, q):
+    """Runs the check script with host"""
     while True:
         # Get a host from the queue
-        host = queue.get()
-        # Prepare the arguments for execution <<missing the host logic>>
-        args = shlex.split(params)
-        process = Popen(args, shell=False, stdout=PIPE, stderr=PIPE)
-        # Put only stdout in the log queue
-        logq.put(process.communicate()[0])
+        host = q.get()
+        parm = [p.replace('%var%',host) for p in params]
+        print('Thread-' + str(i) + ' Processing: ' + host + '\n')
+        pcheck = Popen(parm, shell=False, stdout=PIPE, stderr=PIPE)
+        
+        pout = pcheck.communicate()[0]
+        print('Thread-'+str(i) + 'Processing: Done\n')
+        logq.put(pout)
+        print('logq size:' + str(logq.qsize()))
+        q.task_done()
 
 # Start the worker threads
-for threadid in range(threads):
-    worker = Thread(target=runner, args=(threadid, hostq))
-    worker.setDaemon = True
+for i in range(arg.threads):
+    worker = Thread(target=check_cert, args=(i, hostq))
+    worker.setDaemon(True)
     worker.start()
 
-# Fill the queue
+# Fill in the queue
 for host in hosts:
     hostq.put(host)
 
-# Wait for the worker threads to finish all the tasks
 hostq.join()
 
-# Put the logs in a list
+
 while True:
     try:
-        msgs.append(logq.get_nowait())
+        log.append(logq.get_nowait())
     except Queue.Empty:
         break
 
-# Print the log
-for msg in msgs:
-    print(msg)
+for entry in log:
+    print(entry)
